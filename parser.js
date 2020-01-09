7deaaed3aa145fe9e61b09400743fd39257d2119
Nahuel Vazquez <nahuel.vazquez@fidtech.net>
2020-01-09 16:44:45 -0300

    feat(write-disk) add a feature that writes the result to a file

--- parser.js
+++ parser.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const {MongoClient} = require('mongodb');
 const turf = require('@turf/turf');
+const path = require('path');
 
 const SOURCE_FILE = './source/departamentos-argentina.json';
 const TARGET_COLLECTION_NAME = 'AR_GEOMETRY';
 const TARGET_DATABASE_URI = 'mongodb://localhost:27017/AR_GEOMETRY';
+const TARGET_DIR_PATH = './parsed';
 
 async function parse() {
     console.log(`Reading file ${SOURCE_FILE}...`);
@@ -41,25 +43,39 @@ async function parse() {
         }
     }
 
-    // insert into the database
+    // write to database and disk
+    console.log(`Writing to database and disk...`);
+    const documents = [];
+    for (const key in provinces) {
+        if (provinces.hasOwnProperty(key)) {
+            const provinceName = key;
+            const province = provinces[provinceName];
+            const document = {
+                name: provinceName,
+                geometry: province.geometry,
+            };
+            documents.push(document);
+        }
+    }
+    // write to disk
+    try {
+        await fs.promises.access(TARGET_DIR_PATH)
+    } catch (e) {
+        if (e && e.code === 'ENOENT') {
+            await fs.promises.mkdir(TARGET_DIR_PATH); // Create dir if it does not exist
+        }
+    }
+    const diskPath = path.join(TARGET_DIR_PATH, 'AR_PROVINCES_GEOMETRY.json');
+    await fs.promises.writeFile(diskPath, JSON.stringify(documents), {encoding: 'utf8'});
+    console.log(`Data successfully written to disk: ${diskPath}`);
+    // write to database
     console.log(`Connecting to database ${TARGET_DATABASE_URI}...`);
     const mongoClient = await MongoClient.connect(TARGET_DATABASE_URI, {useUnifiedTopology: true});
     console.log(`Creating collection ${TARGET_COLLECTION_NAME}...`);
     await mongoClient.db().collection(TARGET_COLLECTION_NAME).drop();
     await mongoClient.db().createCollection(TARGET_COLLECTION_NAME);
-    for (const key in provinces) {
-        if (provinces.hasOwnProperty(key)) {
-            const provinceName = key;
-            const province = provinces[provinceName];
-            console.log(`Inserting province ${provinceName}...`);
-            await mongoClient.db().collection(TARGET_COLLECTION_NAME).insertOne({
-                name: provinceName,
-                geometry: province.geometry,
-            });
-        }
-    }
-    console.log(`Data parsed and written to ${TARGET_DATABASE_URI}, ${TARGET_COLLECTION_NAME}`);
-
+    await mongoClient.db().collection(TARGET_COLLECTION_NAME).insertMany(documents);
+    console.log(`Data successfully written to database: ${TARGET_DATABASE_URI}, ${TARGET_COLLECTION_NAME}`);
 }
 
 console.log('Starting script...');
